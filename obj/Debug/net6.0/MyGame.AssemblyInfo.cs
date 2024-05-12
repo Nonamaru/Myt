c0fb634c02b96f4f87a4cbaf553b5bf9acaa575a
Nonamaru <zykinf@gmail.com>
2024-05-12 12:28:44 +0500

    some changes

--- obj/Debug/net6.0/MyGame.AssemblyInfo.cs
+++ obj/Debug/net6.0/MyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f2e70d909dd80d6d27a093aede128088e425fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a631eb18c4a4de4ae3253728b68688f3eebd098")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
